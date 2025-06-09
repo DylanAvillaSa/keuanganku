dc138eefed424dde18e4e6379b4ee62316d1408d
dylanavilla1 <senseilevi@gmail.com>
2025-06-09 16:46:57 +0700

    mengubah code dan tampilan

--- src/app/head.js
+++ src/app/head.js
@@ -44,7 +44,10 @@ export default function Head() {
 
       {/* Buat iOS full screen mode */}
       <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
       <meta name="apple-mobile-web-app-title" content="Keuangangku" />
     </>
   );
